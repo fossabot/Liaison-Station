5afc799dd8b0f2457d9f59b4be99c7dd3160a402
ZhangWentao-Github <mailbox@zhang-wen-tao.com>
2025-02-27 22:52:23 +0800

    update

--- src/app/components/ui/highlighter/langs/index.ts
+++
@@ -1,5 +0,0 @@
-import { LanguageRegistration } from '@shikijs/types';
-
-import HTML from './html.json';
-
-export const SHIKI_LANGS = [HTML] as unknown as LanguageRegistration[];
--- src/app/components/ui/highlighter/themes/index.ts
+++
@@ -1,5 +0,0 @@
-import { ThemeRegistrationAny } from '@shikijs/types';
-
-import oneDarkPro from './one-dark-pro.json';
-
-export const SHIKI_THEME = [oneDarkPro] as unknown as ThemeRegistrationAny[];
